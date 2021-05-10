37e21c7bca2978e60fe5f268a9d9ba967fe7b28d
ShanGuangFF <909278799@qq.com>
2021-05-10 16:24:28 +0800

    初始化

---
+++ src/main/java/com/example/tinyblog/TinyblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyblogApplication.class, args);
+    }
+
+}
